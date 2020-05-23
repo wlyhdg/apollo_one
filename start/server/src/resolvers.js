a76e1b10be11fc87cd8400c4c6610d8e5b7b10f2
Wael Yehdego <waelwc3@gmail.com>
2020-05-23 12:00:26 -0700

    complete launch query resolvers

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,7 +1,47 @@
+const { paginateResults } = require('./utils');
+
 module.exports = {
   Query: {
-    launches: (_, __, { dataSources }) => dataSources.launchAPI.getAllLaunches(),
+    // launches: (_, __, { dataSources }) => dataSources.launchAPI.getAllLaunches(),
+    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      // we want these in reverse chronological order
+      allLaunches.reverse();
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches
+      });
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null,
+        hasMore: launches.length ? 
+          launches[launches.length - 1].cursor !== allLaunches[allLaunches.length - 1].cursor 
+          : false
+      };
+    },
     launch: (_, { id} , { dataSources }) => dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+  },
+  Mission: {
+    missionPatch: () => {
+      return size === 'SMALL' ? mission.missionPatchSmall: mission.missionPatchLarge;
+    }
+  },
+  Launch: {
+    isBooked: async (launch, _, { dataSources }) => dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id })
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      // get ids of launches by user
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+      if (!launchIds.length) return [];
+      // look up those launches by their ids
+      return (
+        dataSources.launchAPI.getLaunchById({
+          launchIds
+        }) || []
+      );
+    }
   }
 }
