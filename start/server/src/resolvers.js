2204556429c90b50461adf9b7766d92b28b0c9c7
Wael Yehdego <waelwc3@gmail.com>
2020-05-24 10:18:25 -0700

    write mutation resolvers

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -23,8 +23,47 @@ module.exports = {
     launch: (_, { id} , { dataSources }) => dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
   },
+  Mutation: {
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) return Buffer.from(email).toString('base64');
+    },
+
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const results = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds
+      });
+
+      return {
+        success: results && results.length === launchIds.length,
+        message: results.length === launchIds.length ? 
+        'trips booked successfully'
+        : `the following launches couldn't be booked: ${launchIds.filter( id => !results.includes(id) )}`,
+        launches
+      }
+    },
+
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = await dataSources.userAPI.cancelTrip({ launchId });
+      
+      if (!result)
+        return {
+          success: false,
+          message: 'failed to cancel trip'
+        };
+
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: 'trip cancelled',
+        launches: [launch]
+      };
+    }
+
+  },
   Mission: {
-    missionPatch: () => {
+    missionPatch: (mission, { size } = {size: 'LARGE' }) => {
       return size === 'SMALL' ? mission.missionPatchSmall: mission.missionPatchLarge;
     }
   },
